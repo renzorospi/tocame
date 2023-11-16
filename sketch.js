9efd08d7c41609741154cc514b38a19268dc4de3
Renzo Rospigliosi <renjor21@gmail.com>
2023-11-15 20:15:11 -0500

    simplificado el texto

--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
+let vid;
 let timer = 0;
 let tiempoInicial;
-
-let vid;
 let playing = false;
 let perdio = false;
-let texto = "";
-let soltar = "";
-let ganaste = "";
+let texto = "tócame";
 let créditos = "Concepto, programación, animación y música: Renzo Rospigliosi";
 let web = "renzorospigliosi.com";
 
@@ -29,59 +26,55 @@ function draw() {
   fill(0);
   textFont("Consolas");
 
-  // Verifica si el jugador mantiene presionado el mouse y el temporizador llega a 60
-  if (mouseIsPressed && timer > 60) {
-    ganaste = "sabía que no me dejarías ir"
-    text(ganaste, width / 2, height / 2);
-    soltar = "";
-    texto = "";
-    textSize(20);
-    text(créditos, width / 2, height - 40);
-    textSize(10);
-    text(web, width / 2, height - 20);
-  } else if (!perdio && timer < 61) {
-    soltar = "";
-    ganaste = "";
-    texto = "tócame"
-    text(texto, width / 2, height / 6);
-  } else {
-    soltar = "qué fácil fue soltarme"
-    ganaste = "";
-    texto = "";
-    text(soltar, width / 2, height / 2);
-    textSize(15);
-    text(créditos, width / 2, height - 40);
-    textSize(10);
-    text(web, width / 2, height - 20);
-  }
-
   if (playing && timer < 61) {
     image(vid, width / 2, height / 2, width / 2, height / 2);
     timer = (millis() - tiempoInicial) / 1000;
     text(floor(timer), width / 2 + 270, height / 6);
+    mostrarTexto("no te alejes", height / 6);
+  } else {
+    if (!playing && !perdio) {
+      mostrarTexto("tócame", height / 6);
+      } else if (playing && timer <= 60) {
+    } else if (playing && timer > 60) {
+      mostrarTexto("sabía que no me dejarías ir", height / 2);
+      mostrarTexto(créditos, height - 40, 15);
+      mostrarTexto(web, height - 20, 10);
+    } else if (!playing && perdio ) {
+      mostrarTexto("qué fácil fue soltarme", height / 2);
+      mostrarTexto(créditos, height - 40, 15);
+      mostrarTexto(web, height - 20, 10);
+    }
   }
 }
 
 function mousePressed() {
-  vid.play();
-  playing = true;
-  texto = "no me sueltes";
-  tiempoInicial = millis();
-
-  // Para resetear el sketch si se cumple alguna de las condiciones
-  if (perdio || timer > 60) {
-    perdio = false;
-    timer = 0;
+  if (!playing) {
+    vid.play();
+    playing = true;
+    texto = "no me sueltes";
     tiempoInicial = millis();
+  } else {
+    // Para resetear el sketch si se cumple alguna de las condiciones
+    if (perdio || timer > 60) {
+      perdio = false;
+      timer = 0;
+      tiempoInicial = millis();
+    }
   }
 }
 
 function mouseReleased() {
-  vid.stop();
-  playing = false;
-  tiempoInicial = 0;
-  texto = "";
-  perdio = true;
+  if (playing) {
+    vid.stop();
+    playing = false;
+    tiempoInicial = 0;
+    perdio = true;
+  }
+}
+
+function mostrarTexto(txt, posY, size = 40) {
+  textSize(size);
+  text(txt, width / 2, posY);
 }
 
 function windowResized() {
--- tocame/tocame.pde
+++ tocame/tocame.pde
@@ -5,19 +5,20 @@ int timer = 0;
 int tiempoInicial;
 
 Movie vid;
-boolean playing;
+boolean playing = false;
 boolean perdio = false;
-String texto = "tócame";
-String soltar = "qué fácil fue soltarme";
-String ganaste = "sabía que no me dejarías ir";
+String texto = "";
+String soltar = "";
+String ganaste = "";
 String créditos = "Concepto, programación, animación y música: Renzo Rospigliosi";
 String web = "renzorospigliosi.com";
 
 void setup() {
   size(displayWidth, displayHeight);
-  vid = new Movie(this, "tocame.mp4");  
+  vid = new Movie(this, "tocame.mp4");
   SoundFile song = new SoundFile(this, "water.mp3");
   song.play();
+  vid.hide();
 }
 
 void draw() {
@@ -27,31 +28,37 @@ void draw() {
   textSize(40);
   fill(0);
   textFont(createFont("Consolas", 40));
-  
-  if (perdio == false && timer < 61){
-    text(texto, width/2, height/6);
-  } else {
-    text(soltar, width/2, height/2);
+
+  // Verifica si el jugador mantiene presionado el mouse y el temporizador llega a 60
+  if (mousePressed && timer > 60) {
+    ganaste = "sabía que no me dejarías ir";
+    text(ganaste, width / 2, height / 2);
+    soltar = "";
+    texto = "";
     textSize(20);
-    text(créditos, width/2, height - 40);
+    text(créditos, width / 2, height - 40);
     textSize(10);
-    text(web, width/2, height -20);
-  }
-  
-  if (playing && timer < 61){
-    image(vid, width/2, height/2, width/2, height/2);
-    timer = (millis() - tiempoInicial) / 1000;
-    text(timer, width/2+270, height/6);
+    text(web, width / 2, height - 20);
+  } else if (!perdio && timer < 61) {
+    soltar = "";
+    ganaste = "";
+    texto = "tócame";
+    text(texto, width / 2, height / 6);
   } else {
-    // Qué pasa cuando no está reproduciendo el video
+    soltar = "qué fácil fue soltarme";
+    ganaste = "";
+    texto = "";
+    text(soltar, width / 2, height / 2);
+    textSize(15);
+    text(créditos, width / 2, height - 40);
+    textSize(10);
+    text(web, width / 2, height - 20);
   }
 
-  // Qué pasa cuando pasa el minuto
-  if (timer > 60){
-    text(ganaste, width/2, height/2);
-    soltar = "";
-  } else {
-    // Qué pasa cuando no ha pasado el minuto
+  if (playing && timer < 61) {
+    image(vid, width / 2, height / 2, width / 2, height / 2);
+    timer = (millis() - tiempoInicial) / 1000;
+    text(floor(timer), width / 2 + 270, height / 6);
   }
 }
 
